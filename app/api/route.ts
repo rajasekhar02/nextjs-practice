bee42aa19e16fad7245cd4a47e167f333752ad35
rajasekhar02 <raja02sekhar08@gmail.com>
2023-09-21 16:22:15 -0500

    2 files modified

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,8 +1,14 @@
 import { NextResponse } from 'next/server';
+export const dynamic = 'auto';
+export const dynamicParams = true;
+export const revalidate = false;
+export const fetchCache = 'auto';
+export const runtime = 'nodejs';
+export const preferredRegion = 'auto';
 
 export async function GET(request: Request) {
   const data = await fetch('https://jsonplaceholder.typicode.com/todos/1', {
     cache: 'force-cache',
   }).then((data) => data.json());
-  return NextResponse.json(JSON.stringify(data));
+  return NextResponse.json(data);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import type { InferGetStaticPropsType } from 'next';
+'use server';
+
 interface TodoItem {
   completed: boolean;
   id: number;
@@ -6,19 +7,21 @@ interface TodoItem {
   userId: number;
 }
 
-async function fetchData(params: { id: string }) {
-  const res = await fetch('/api/', { cache: 'no-store' });
-  const data = await res.json();
+async function fetchData(params: { id: string }): Promise<TodoItem> {
+  const res: Response = await fetch('http://localhost:3000/api', {
+    method: 'GET',
+    cache: 'no-store',
+  });
+  const data: TodoItem = await res.json();
   return data;
 }
 
 export default async function Page() {
   const data = await fetchData({ id: '1' });
-
   return (
     <div className="space-y-4">
-      <h1 className="text-2xl font-medium text-gray-200">{data.title}</h1>
-      <p className="font-medium text-gray-500">{data.body}</p>
+      <h1 className="text-2xl font-medium text-gray-200">{data.userId}</h1>
+      <p className="font-medium text-gray-500">{data.title}</p>
     </div>
   );
 }
