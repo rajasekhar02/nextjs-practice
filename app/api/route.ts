104f72fc61bdd3322f8ea63e641cd69fb23a48f3
rajasekhar <raja02sekhar08@gmail.com>
2023-09-21 20:31:18 -0500

    feat: pagination working

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next/server';
+import { NextRequest,NextResponse } from 'next/server';
 export const dynamic = 'auto';
 export const dynamicParams = true;
 export const revalidate = false;
@@ -6,8 +6,10 @@ export const fetchCache = 'auto';
 export const runtime = 'nodejs';
 export const preferredRegion = 'auto';
 
-export async function GET(request: Request) {
-  const data = await fetch('https://jsonplaceholder.typicode.com/todos/1', {
+export async function GET(request: NextRequest) {
+  const page = request.nextUrl.searchParams.get('page')
+  const URL = `https://rickandmortyapi.com/api/character/?${new URLSearchParams(`page=${page}`)}`
+  const data = await fetch(URL, {
     cache: 'force-cache',
   }).then((data) => data.json());
   return NextResponse.json(data);
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,63 @@
 'use server';
 
-interface TodoItem {
-  completed: boolean;
+interface Character {
   id: number;
-  title: string;
-  userId: number;
+  name: string;
+  status: 'Alive'|'Dead'|'unknown';
+  gender: 'Male'|'Female'
+  image: string
 }
 
-async function fetchData(params: { id: string }): Promise<TodoItem> {
-  const res: Response = await fetch('http://localhost:3000/api', {
+type Info = {
+  count: number,
+pages: number,
+next: string,
+prev: string
+}
+interface ListCharacterResponse
+{
+  info : Info;
+  results: Character[];
+}
+
+type URLParams = {
+  page: number
+}
+
+async function fetchData({page}: { page: number }): Promise<ListCharacterResponse> {
+  const URL = `http://localhost:3000/api?${new URLSearchParams(`page=${page}`)}`
+  console.log(URL)
+  const res: Response = await fetch(URL, {
     method: 'GET',
     cache: 'no-store',
   });
-  const data: TodoItem = await res.json();
+  const data: ListCharacterResponse = await res.json();
+  data.info.next = `?page=${page+1 > data.info.pages ? 1:page+1}`
+  data.info.prev = `?page=${page-1 == 0 ? data.info.pages: page-1}`
   return data;
 }
 
-export default async function Page() {
-  const data = await fetchData({ id: '1' });
+export default async function Page({searchParams}:{searchParams:URLParams}) {
+  console.log(searchParams)
+  const data = await fetchData({ page: searchParams.page||1 });
+  const {info, results} = data
   return (
     <div className="space-y-4">
-      <h1 className="text-2xl font-medium text-gray-200">{data.userId}</h1>
-      <p className="font-medium text-gray-500">{data.title}</p>
+      <div className="pagination-actions">
+        {<a href={info.next}>Next</a>}
+        {<a href={info.prev}>Prev</a>}
+      </div>
+      <ul>
+        {
+          results.map(character => (
+            <li>
+              <h1 className="text-2xl font-medium text-gray-200">{character.name}</h1>
+              <p className="font-medium text-gray-500">{character.status}</p>
+              <img src={character.image}></img>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
