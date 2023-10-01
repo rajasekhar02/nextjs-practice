5afe1bb91378859f425b3341221aae14d51947c1
rajasekhar <raja02sekhar08@gmail.com>
2023-09-30 22:32:44 -0500

    feat: add typing for useSetAtom

--- apps/delimiter-viewer/src/app/components/Step1Form.tsx
+++ apps/delimiter-viewer/src/app/components/Step1Form.tsx
@@ -3,55 +3,59 @@ import RadioButton from "./RadioButton";
 import { FormStages, Values } from "../types";
 import TextArea from "./TextArea";
 import { formStagesAtom, step1FormAtom } from "../state";
+import { ExtractAtomValue, useAtomValue, ExtractAtomArgs, ExtractAtomResult } from 'jotai'
 import { useSetAtom } from "jotai";
 
+type SetAtom<Args extends any[], Result> = (...args: Args) => Result
 
-  
-  const Step1Form = function () {
-    const defaultDelimiters: Array<[string, string]> = [
-      ["Comma", ","],
-      ["pipe", "|"],
-      ["tab", " "],
-    ];
-    let formInitValues = {
-      delimiter: defaultDelimiters[1][1],
-      delimitedText: "",
-    };
-    let setStep1Form = useSetAtom(step1FormAtom)
-    let setFormStage = useSetAtom(formStagesAtom)
-    function submitStep1Form(
-      values: Values,
-      {setSubmitting}:FormikHelpers<Values>
-    ) {
-      setStep1Form(values)
-      setFormStage({stage:FormStages.Step2Form})
-      // setTimeout(() => {
-      //   alert(JSON.stringify(values, null, 2));
-      //   setSubmitting(false);
-      // }, 500);
-    }
-    return (
-      <Formik initialValues={formInitValues} onSubmit={submitStep1Form}>
-        <Form>
-          <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">
-            Delimiter
-          </h3>
-          <ul className="radio-group-container w-full items-center rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white sm:flex">
-            {defaultDelimiters.map((delimiter, index: number) => {
-              return (
-                <RadioButton
-                  label={delimiter[0]}
-                  value={delimiter[1]}
-                  key={`delimiter-radio-${index}`}
-                ></RadioButton>
-              );
-            })}
-          </ul>
-          <TextArea></TextArea>
-          <button type="submit">Next</button>
-        </Form>
-      </Formik>
-    );
+function submitStep1Form(
+  values: Values,
+  {setSubmitting}:FormikHelpers<Values>,
+  setStep1Form: SetAtom<ExtractAtomArgs<typeof step1FormAtom>,ExtractAtomResult<typeof step1FormAtom>>,
+  setFormStage:SetAtom<ExtractAtomArgs<typeof formStagesAtom>,ExtractAtomResult<typeof formStagesAtom>>
+) {
+  setStep1Form(values)
+  setFormStage({stage:FormStages.Step2Form})
+  // setTimeout(() => {
+  //   alert(JSON.stringify(values, null, 2));
+  //   setSubmitting(false);
+  // }, 500);
+}
+
+const Step1Form = function () {
+  const defaultDelimiters: Array<[string, string]> = [
+    ["Comma", ","],
+    ["pipe", "|"],
+    ["tab", " "],
+  ];
+  let formInitValues = {
+    delimiter: defaultDelimiters[1][1],
+    delimitedText: "",
   };
+  let setStep1Form = useSetAtom(step1FormAtom)
+  let setFormStage = useSetAtom(formStagesAtom)
+  return (
+    <Formik initialValues={formInitValues} onSubmit={(values, params)=>submitStep1Form(values, params, setStep1Form, setFormStage)}>
+      <Form>
+        <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">
+          Delimiter
+        </h3>
+        <ul className="radio-group-container w-full items-center rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white sm:flex">
+          {defaultDelimiters.map((delimiter, index: number) => {
+            return (
+              <RadioButton
+                label={delimiter[0]}
+                value={delimiter[1]}
+                key={`delimiter-radio-${index}`}
+              ></RadioButton>
+            );
+          })}
+        </ul>
+        <TextArea></TextArea>
+        <button type="submit">Next</button>
+      </Form>
+    </Formik>
+  );
+};
   
   export default Step1Form
